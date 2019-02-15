684b65e9bd1c971b8f9f5dcfb28e6151193e6898
Maxime Salomon <maxime@croissant.io>
2019-02-15 10:56:20 +0100

    mvp done

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Table from './components/Table.jsx'
 
 class App extends Component {
   constructor() {
@@ -33,6 +34,9 @@ class App extends Component {
     return (
       <div className="App">
         <h1 className="Header">React Wars</h1>
+        < Table
+          starwarsChars={this.state.starwarsChars}
+        />
       </div>
     );
   }
